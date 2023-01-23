3ea0a57b55a57042b25c007e1869e1f168f9c8df
Senjuti De <75435467+Senjuti256@users.noreply.github.com>
2023-01-23 23:01:52 +0530

    Create soln6.go

---
+++ soln6.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Print("1) Senjuti ")
+	fmt.Print("De\n")
+
+	fmt.Println("--------------------")
+
+	fmt.Println("2) Senjuti")
+	fmt.Println("   De")
+
+	fmt.Println("--------------------")
+
+	name := "Senjuti De"
+	fmt.Printf("My name is %v", name)
+
+	fmt.Println("\n--------------------")
+}
