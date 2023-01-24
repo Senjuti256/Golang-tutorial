726e2d8f2dcfabf1dc4df3d9e395db5c6268caf7
Senjuti De <75435467+Senjuti256@users.noreply.github.com>
2023-01-24 09:34:14 +0530

    Update soln5.go

--- soln5.go
+++ soln5.go
@@ -2,15 +2,23 @@ package main
 
 import "fmt"
 
+func product(x int, y int, z int) int {
+	var product int
+	product = x * y * z
+	return product
+}
+
 func main() {
 
-	var (
-		product int
-		a, b, c = 3, 6, 3
-	)
+	//var (
+		//product int
+		//a, b, c = 3, 6, 3
+	//)
+	//product = a * b * c
+	//fmt.Println(product)
 
-	product = a * b * c
-
-	fmt.Println(product)
+	
+	
+	fmt.Println(product(2, 4, 6))
 
 }
