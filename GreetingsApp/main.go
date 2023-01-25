b7586e34d07712a4eb23fbb2c697ef5815ea0827
Senjuti De <75435467+Senjuti256@users.noreply.github.com>
2023-01-25 12:06:38 +0530

    Create main.go

---
+++ GreetingsApp/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"GreetingsApp/pack"
+)
+
+var s string
+
+func main(){
+	fmt.Println("Welcome to my app!")
+	fmt.Println("........................")
+	//fmt.Println("Enter your name:")
+	pack.Hello("Tom")
+}
